7d5c8d68e295f2b01aed8508f83894de05450b19
Diego122004 <daof.04@mail.com>
2025-06-27 15:37:51 -0600

    Primera funcion

--- Exm2P3_DiegoOrdonez.cpp
+++ Exm2P3_DiegoOrdonez.cpp
@@ -1,19 +1,69 @@
-// Exm2P3_DiegoOrdonez.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
+#include"Jugador.h"
+#include"Apuesta.h"
 #include <iostream>
+using namespace std;
+
+void registrar_Nuevo_Jugador(vector<Jugador*> casino) {
+    vector<Apuesta*> apuestas;
+    int id, saldo,level;
+    string name;
+
+    cout << "ingrese el Id del jugador";
+    cin >> id;
+    cout << "ingrese el nombre del Jugador";
+    cin >> name;
+    cout << "Ingrese el saldo inicias del Jugador";
+    cin >> saldo;
+    cout << "Ingrese la hablilidad del Jugador";
+    cin >> level;
+
+   // Jugador = new Jugador(apuestas, id, name, saldo, level)
+    casino.push_back(new Jugador(apuestas, id, name, saldo, level));
+    
+};
+
+void menu()
+{
+    int opcion;
+    do {
+        cout << "MENU" << endl;
+        cout << "1. Registrar nuevo jugador" << endl;
+        cout << "2. Listar Jugador " << endl;
+        cout << "3. Eliminar Jugador" << endl;
+        cout << "4. Buscar Jugador por ID" << endl;
+        cout << "5. Registrar una apuesta " << endl;
+        cout << "6. Guardar Jugadores" << endl;
+        cout << "7. Cargar Jugadoers" << endl;
+        cout << "8. Salir" << endl;
+
+        cin >> opcion;
+        switch (opcion != 0)
+        {
+        case 0: {
+
+            break;
+        };
+
+        case 1: {
+            
+
+
+        };
+              break;
+        case 2: {
+            
+        };
+              break;
+        default:
+            menu();
+            break;
+        };
+    } while (opcion != 0);
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector<Jugador*> casino;
+    menu();
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
